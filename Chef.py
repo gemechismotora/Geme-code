6f7a849bf035e173ccb8dd7265e370bdadcded49
Gemechis <gmtekalegn588@gmail.com>
2024-10-21 10:58:05 +0300

    github

---
+++ Chef.py
@@ -0,0 +1,13 @@
+class Chef:
+
+    def make_chicken(self):
+        print("The Chef  makes a chicken")
+
+    def make_salad(self):
+        print("The chef makes a salad")
+
+    def make_special_disk(self):
+        print("The chef makes bq ribs")
+
+    def make_kitfo(self):
+        print("The chef makes kitfo")
